c5e786672f1d7b605d14e9fd042a28d04e60ddc6
Kaddek-Bayu-Bholedt <kaddekbayu444@gmail.com>
2021-03-09 18:59:42 +0700

    function overriding

--- Visibility.php
+++ Visibility.php
@@ -7,6 +7,9 @@ $product = new Product("Asus", 1000000);
 echo $product->getName() . PHP_EOL;
 echo $product->getPrice() . PHP_EOL;
 
+$product->getInfo();
+
 $dummy = new ProductDummy("Lenovo", 1000000);
 
-$dummy->Info();
+$dummy->getInfo();
+$dummy->getParentInfo();
--- data/Product.php
+++ data/Product.php
@@ -9,6 +9,10 @@ class Product{
         $this->name = $name;
         $this->price = $price;
     }
+    public function getInfo(){
+        echo "Product Name Master : $this->name" . PHP_EOL;
+        echo "Product Price Master : $this->price" . PHP_EOL;
+    }
     public function getName(){
         return $this->name;
     }
@@ -19,8 +23,11 @@ class Product{
 }
 
 class ProductDummy extends Product{
-    public function Info(){
-        echo $this->name . PHP_EOL;
-        echo $this->price . PHP_EOL;
+    public function getInfo(){
+        echo "Product Name Dummy : $this->name" . PHP_EOL;
+        echo "Product Price Dummy : $this->price" . PHP_EOL;
+    }
+    public function getParentInfo(){
+        echo parent::getInfo();
     }
 }
