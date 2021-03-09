4c081650aaa1af19b637f7868a04de5d1a27ca09
Kaddek-Bayu-Bholedt <kaddekbayu444@gmail.com>
2021-03-09 15:34:54 +0700

    Type data pada properties

--- object.php
+++ object.php
@@ -5,9 +5,6 @@ require_once "data/person.php";
 $person = new Person();
 $person->name = "Bayu";
 $person->address = "Rahayu";
-$person->country = "Indonesia";
-
-var_dump($person);
 
 echo "Name : $person->name" . PHP_EOL;
 echo "Address : $person->address" . PHP_EOL;
