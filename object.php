cba77bd1e65288a055b1df82b4cf06db00918b98
Kaddek-Bayu-Bholedt <kaddekbayu444@gmail.com>
2021-03-09 15:40:38 +0700

    function pada class

--- object.php
+++ object.php
@@ -9,3 +9,7 @@ $person->address = "Rahayu";
 echo "Name : $person->name" . PHP_EOL;
 echo "Address : $person->address" . PHP_EOL;
 echo "Country : $person->country" . PHP_EOL;
+
+$person2 = new Person();
+
+$person->sayHello("Bayu");
