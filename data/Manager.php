611054fc9aef835b5961eff72f602b1a4a624ac2
Kaddek-Bayu-Bholedt <kaddekbayu444@gmail.com>
2021-03-09 19:13:44 +0700

    construct overriding

--- data/Manager.php
+++ data/Manager.php
@@ -1,13 +1,23 @@
 <?php
 
 class Manager{
-    var string $name;
+    public string $name;
+    public string $title;
 
-    function sayHello(string $name){
-        echo "Hi $name, my name is $this->name" . PHP_EOL;
+    public function __construct(string $name = "", string $title = "Manager")
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
+
+    public function sayHello(string $name){
+        echo "Hi $name, my name is $this->name , my title is $this->title" . PHP_EOL;
     }
 }
 
 class vicePresident extends Manager{
-
+    public function __construct(string $name = "")
+    {
+        parent::__construct($name, "VP");
+    }
 }
